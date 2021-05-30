69154f5ac5e68cb23d4f415da7af5e1a56a5c2c9
prakx <prakx2050@gnail.com>
2021-05-30 22:58:18 +0530

    Added Explore UI

--- src/components/explore.js
+++ src/components/explore.js
@@ -1,108 +1,129 @@
-import React ,{Component} from 'react';
+import React, { Component } from "react";
 import DataListInput from "react-plain-datalist-input";
 
-import firebase from '../helper/firebase'
+import firebase from "../helper/firebase";
+import { Result } from "./Result";
+import "../components_css/explore.css";
 
+class Explore extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { occupation: "", keywords: "", providers: [], found: 1 };
 
-class Explore extends Component{  
+    this.loadkeywords = this.loadkeywords.bind(this);
+    this.occupationOnChangeHandler = this.occupationOnChangeHandler.bind(this);
+    this.go = this.go.bind(this);
+  }
 
-constructor(props){
-      super(props);
-      this.state = {occupation:'',keywords:''};
-    
-      this.loadkeywords=this.loadkeywords.bind(this);
-      this.occupationOnChangeHandler=this.occupationOnChangeHandler.bind(this);
-      this.go=this.go.bind(this);
-     
-     
+  async componentWillMount() {
+    var items = [{ item: "fetching.." }];
+    this.setState({ keywords: items });
+    await this.loadkeywords();
+  }
 
-};
-  
-  
-  
-async componentWillMount(){
-    var items=[{item:'fetching..'}]
-    this.setState({keywords:items});
-      await this.loadkeywords()
-      
-}
+  async loadkeywords() {
+    firebase
+      .firestore()
+      .collection("occupations")
+      .get()
+      .then((snapshot) => {
+        let array = [];
+        snapshot.docs.forEach((doc) => {
+          var Item = {
+            key: doc.data().key,
+            item: doc.data().item,
+          };
+          array.push(Item);
+        });
 
-async loadkeywords(){
-    firebase.firestore().collection('occupations').get().then((snapshot=>{
-      let array=[];
-      snapshot.docs.forEach((doc=>{
-          var Item={
-              key:doc.data().key,
-              item:doc.data().item
-          }
-          array.push(Item)
-          
-      }))
-
-      this.setState({
-          keywords:array
+        this.setState({
+          keywords: array,
+        });
       })
- 
+      .catch((err) => {
+        console.log("error : " + err);
+      });
+  }
 
+  occupationOnChangeHandler(event) {
+    this.setState({ occupation: event.target.value });
+    console.log(this.state.occupation);
+  }
 
-        })).catch((err)=>{
-            console.log("error : "+err)
+  go() {
+    if (this.state.occupation === "") {
+      alert("Enter in the search box!");
+    } else {
+      console.log(this.state.occupation);
+      firebase
+        .firestore()
+        .collection("serviceProviders")
+        .where("occupation", "==", this.state.occupation.toLowerCase())
+        .get()
+        .then((snapshot) => {
+          console.log("exce");
+
+          var array1 = [];
+          console.log("LENGTH" + snapshot.docs.length);
+          if (snapshot.docs.length === 0) {
+            this.setState({ found: 0 });
+          } else {
+            this.setState({ found: 1 });
+
+            snapshot.docs.forEach((doc) => {
+              console.log(doc.data());
+              var provider = {
+                name: doc.data().name,
+                location: doc.data().location,
+                phone: doc.data().phone,
+                uid: doc.data().uid,
+              };
+              array1.push(provider);
+              // this.state.providers.push(provider)
+              console.log("exce1");
+              console.log("name:" + doc.data().name);
+              //code to show service providers
+              this.setState({ providers: array1 });
+            });
+          }
         })
-}
+        .catch((err) => {
+          console.log("error : " + err);
+        });
+    }
+  }
 
-occupationOnChangeHandler(event){
-    this.setState({occupation: event.target.value});
-    console.log(this.state.occupation)
-}
-
-
-go(){
-    console.log(this.state.occupation)
-    firebase.firestore().collection('serviceProviders').where('occupation','==',this.state.occupation).get().then((snapshot=>{
-        console.log("exce")
-        snapshot.docs.forEach((doc=>{
-            console.log("exce1")
-            console.log("name:"+doc.data().name)
-            //code to show service providers
-
-            
-
-        }))
-  
-        
-   
-  
-  
-          })).catch((err)=>{
-              console.log("error : "+err)
-          })
-
-}
-
-
-
-  
-render(){
-             
-      return (
+  render() {
+    return (
       <div>
-             <form>
-             <input list="occupation" onChange={this.occupationOnChangeHandler} />
-                    <datalist id="occupation" >
-                        {this.state.keywords.map((t) =>
-                        <option key={t.key}  value={t.item} />
-                        )}
-                    </datalist>
-            </form>
-            <a href="#" onClick={this.go} >Submit</a>
-            
+        <div className="form">
+          <form>
+            <input
+              list="occupation"
+              value={this.state.occupation}
+              onChange={this.occupationOnChangeHandler}
+            />
+            <datalist id="occupation">
+              {this.state.keywords.map((t) => (
+                <option key={t.key} value={t.item} />
+              ))}
+            </datalist>
+          </form>
+          <button className="Search" onClick={this.go}>
+            <i class="fas fa-search fa-2x"></i>
+          </button>
+        </div>
 
-
+        <div className="results">
+          {this.state.found == 0
+            ? "khud se kr"
+            : this.state.providers.map((provider) => {
+                return <Result provider={provider} />;
+              })}
+        </div>
       </div>
-            );
-  
+    );
+  }
 }
-   
-}
-  
-  export default Explore;
+
+export default Explore;
