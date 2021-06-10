a4ae76d30f2be05817ce9b2a2142927e82845260
Tavishi Suvarna <48873388+Tavishi10@users.noreply.github.com>
2021-06-10 10:01:34 +0530

    Add files via upload

---
+++ src/components/HeroSection.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import '../components_css/HeroSection.css'
+import '../App.css'
+
+function HeroSection() {
+    return (
+        <div className='hero-container'> 
+           <div id="zoomeffect">
+           <h1>WELCOME TO NEAR ME!!</h1>
+            <p>All your utility services in one place!!</p>
+           </div>
+            
+        </div>
+        
+    )
+}
+
+export default HeroSection
